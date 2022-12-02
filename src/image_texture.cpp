e37593228537d88718632c6158113db40cc981d2
gianluca-m <74053868+gianluca-m@users.noreply.github.com>
2022-12-02 18:17:36 +0100

    image-textures: added scale property

--- src/image_texture.cpp
+++ src/image_texture.cpp
@@ -26,15 +26,15 @@ NORI_NAMESPACE_BEGIN
 class ImageTexture : public Texture<Color3f> {
 public:
     ImageTexture(const PropertyList &props) {
-        m_filename = props.getString("filename");
+        m_filename = getFileResolver()->resolve(props.getString("filename")).str();
+        m_scale = props.getVector2("scale", Vector2f(1.0f));
         m_shift = props.getVector2("shift", Vector2f(0.0f));
+        m_linear_rgb = props.getBoolean("linear_rgb", true);
 
-        auto filepath = getFileResolver()->resolve(m_filename);
-
-        m_image = stbi_load(filepath.str().c_str(), &m_width, &m_height, &m_channels, 0);
+        m_image = stbi_load(m_filename.c_str(), &m_width, &m_height, &m_channels, 0);
 
         if (m_image == NULL) {
-            tfm::printfln("Loading image texture '%s' failed", m_filename);
+            tfm::printfln("Error: Loading image texture '%s' failed\n\t%s", m_filename, stbi_failure_reason());
         }
         else {
             tfm::printfln("Loading image texture successful. #channels = %i, width = %i, height = %i", m_channels, m_width, m_height);
@@ -44,30 +44,29 @@ public:
     std::string toString() const {
         return tfm::format(
             "ImageTexture[\n"
-                "  filename = %s\n"
+                "  filename = \"%s\",\n"
+                "  scale = %s,\n"
+                "  shift = %s,\n"
+                "  linear_rgb = %s\n"
                 "]",
-            m_filename);
+            m_filename, m_scale.toString(), m_shift.toString(), m_linear_rgb);
     }
 
     Color3f eval(const Point2f &uv) {
-        auto x = (int) std::round(uv.x() * m_width);
-        auto y = (int) std::round((1.0 - uv.y()) * m_height);
+        // Map uv to texture coordinates
+        auto mappedX = uv.x() * m_width;
+        auto mappedY = (1.0f - uv.y()) * m_height;
 
-        if (x < 0) x = 0; 
-        if (y < 0) y = 0; 
-
-        if (x > m_width - 1) x = m_width - 1;
-        if (y > m_height - 1) y = m_height - 1;
-
-        x = (int) (x + m_shift.x() * m_width) % m_width;        
-        y = (int) (y + m_shift.y() * m_height) % m_height;
+        // Apply scale and shift
+        auto x = ((int) (mappedX * m_scale.x() + m_shift.x() * m_width)) % m_width;
+        auto y = ((int) (mappedY * m_scale.y() + m_shift.y() * m_height)) % m_height;
 
         int index = (x + y * m_width) * m_channels;
         auto r = (float) (m_image[index] / 255.0);
         auto g = (float) (m_image[index + 1] / 255.0);
         auto b = (float) (m_image[index + 2] / 255.0);
 
-        return Color3f{r, g, b};
+        return m_linear_rgb ? Color3f{r, g, b}.toLinearRGB() : Color3f{r, g, b};
     }
 
 protected:
@@ -76,7 +75,9 @@ protected:
     int m_height;
     int m_width;
     int m_channels;
+    Vector2f m_scale;
     Vector2f m_shift;
+    bool m_linear_rgb;
 };
 
 NORI_REGISTER_CLASS(ImageTexture, "image_texture")
